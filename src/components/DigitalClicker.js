38752b4dbb5b62294c7b8ea713958cc031957c3c
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-21 03:13:31 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -10,7 +10,7 @@ export default class DigitalClicker extends React.Component {
 
     update = () => {
       this.setState({
-        timesClicked: 1
+        timesClicked: this.state.timesClicked + 1
       }
       )
     }
