84fae49ee74cd72bef86bcae6ceb2257cd294e3c
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-21 03:56:10 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -15,7 +15,7 @@ export default class YouTubeDebugger extends React.Component {
         }
        }
     this.handleClickRate = this.handleClickRate.bind(this);
-    this.handleClickresolution = this.handleClickresolution.bind(this);
+    this.handleClickResolution = this.handleClickResolution.bind(this);
 
     }
 
