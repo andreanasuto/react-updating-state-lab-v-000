43d01d4b919df37de477be92ff2667d637027b79
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-21 03:11:19 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -13,7 +13,7 @@ export default class DigitalClicker extends React.Component {
 
   render () {
     return (
-      <button onClick={update}> {this.state.timesClicked}
+      <button onClick={update}> {this.state.timesClicked} </button>
     )
   }
 }
