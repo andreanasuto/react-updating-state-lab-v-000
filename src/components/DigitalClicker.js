5822c420285f096aefa3b2f3136c24a94a8b1b56
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-21 04:00:06 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -11,8 +11,7 @@ export default class DigitalClicker extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-
-    function handleClick () {
+    handleClick () {
       this.setState({
         timesClicked: this.state.timesClicked + 1
       })
